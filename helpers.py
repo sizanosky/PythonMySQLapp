2dc197d1d25920fb63e77fd3e7fe35185b8586f5
Marcos Fabricio Sizanosky <fabricio_sizanosky@hotmail.com>
2021-09-08 05:18:54 -0300

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,37 @@
+import mysql.connector
+
+""" helpers.py """
+""""
+ Funções auxiliares do programa.
+Autor: Marcos Fabricio Sizanosky
+Email: fabricio_sizanosky@hotmail.com
+Date: 2021.09.08
+"""
+
+
+def connect_db(host, user, password, name_db=None):
+    """
+    Creates an object connection to MySQL database.
+    :param host: hostname or IP server (string)
+    :param user: username (string)
+    :param password: user password (string)
+    :param name_db: (string)
+    :return: object "my_db"
+    """
+
+    # Verifica se foi passado um DB como parâmetro e se DB já existe.
+    if not name_db:
+        my_db = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=password
+        )
+    else:
+        my_db = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=password,
+            database=name_db
+        )
+
+    return my_db
---
+++ setup_db.py
@@ -0,0 +1,15 @@
+from helpers import connect_db
+
+""" setup.py """
+""""
+ Programa em Python para realizar a conexão com o MySQL server.
+Autor: Marcos Fabricio Sizanosky
+Email: fabricio_sizanosky@hotmail.com
+Date: 2021.09.08
+"""
+
+# Constantes.
+DATABASE_NAME = "controle_financeiro"
+HOST = "localhost"
+USER = "root"
+PASSWORD = "1234"
